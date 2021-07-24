337fe766e18efae388eba3194798843d0634bed8
YoLabs <59382666+yolabs007@users.noreply.github.com>
2021-07-24 10:18:52 +0530

    Add files via upload
    This code is  not only your first lesson with ESP32 but always a quick check to ensure the smooth working of the following.
    1. Your Data Cable 
    2. ESP32 Board 
    3. Arduino IDE
    4. Computer Port 
    
    Hence this is the first code you must run when  you start your day with programming.

---
+++ blink.ino
@@ -0,0 +1,23 @@
+/////////////////////////////////
+// Generated with a lot of love//
+// with TUNIOT FOR ESP8266     //
+// Website: Easycoding.tn      //
+/////////////////////////////////
+
+
+void setup()
+{
+  pinMode(2, OUTPUT);
+
+}
+
+
+void loop()
+{
+
+    digitalWrite(2,HIGH);
+    delay(100);
+    digitalWrite(2,LOW);
+    delay(100);
+
+}
