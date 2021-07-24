3e40580cf62d12fbb798d1812ac70953a08743a5
YoLabs <59382666+yolabs007@users.noreply.github.com>
2021-07-24 12:20:07 +0530

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -1,8 +1,4 @@
-/////////////////////////////////
-// Generated with a lot of love//
-// with TUNIOT FOR ESP8266     //
-// Website: Easycoding.tn      //
-/////////////////////////////////
+
 
 
 void setup()
