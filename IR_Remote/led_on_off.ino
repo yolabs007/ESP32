af3a693c208944c32b088271baeb0fff4d6e0431
YoLabs <59382666+yolabs007@users.noreply.github.com>
2023-08-17 13:00:59 +0530

    Update led_on_off.ino

--- IR_Remote/led_on_off.ino
+++ IR_Remote/led_on_off.ino
@@ -1,8 +1,9 @@
 /*
-This code is written by Yolabs 
-This code will  help you to know the  button and its code and will help you make inbuilt LED on or off for a sec
 
-To control a motor in both directions using an ESP32 and IR remote control, you need to modify the code  
+This code is written by Rahul Sharma for Yolabs 
+This code will  help you to know the  button and its code and will help you make inbuilt LED on or off for a sec as well as test blocks to check your  HEXcode from remote
+
+To control a motor in both directions using an ESP32 and IR remote control, you will need to modify the code or take the code - Remote_Motor Control.ino  
 Hardware:
 ESP32 Development Board -dev kit v1 
 IR Receiver Module & remote control - provided as a set 
@@ -14,21 +15,28 @@ REciver to ESP32 Connection
 
 VCC or + -> 3.3V of ESP32
 GND or - -> GND of ESP32
-OUT or S -> Any GPIO (for this code it is  22)
+OUT or S -> Any GPIO (for this code it is  D15)
 
 */
 
 
-#include <IRremoteESP8266.h>
+#include <IRremoteESP8266.h>  // if not installed please install this library it works for ESP32 also
 #include <IRrecv.h>
 #include <IRutils.h>
 
-#define IR_RECEIVE_PIN 22
+#define IR_RECEIVE_PIN 15
 #define LED_PIN 2
 
-// Sample IR code. Please replace with the one you captured.
-#define BUTTON_PRESS_CODE1 0xE318261B
-#define BUTTON_PRESS_CODE2 0xFFA25D
+// Sample IR codes . Please replace with the one you captured. To know your code use serial monitor.
+
+#define BUTTON1_PRESS_CODE1 0xE318261B
+#define BUTTON1_PRESS_CODE2 0xFFA25D
+#define BUTTON_PRESS_UP 0x3D9AE3F7
+#define BUTTON_PRESS_STOP1 0x488F3CBB
+#define BUTTON_PRESS_STOP2 0xFF38C7
+#define BUTTON_PRESS_DOWN 0x1BCO152B
+
+
 
 
 
@@ -47,15 +55,45 @@ void loop() {
         Serial.print("Received IR Code: 0x");
         Serial.println(results.value, HEX); // This will print the received IR code in HEX format
         
-        if (results.value == BUTTON_PRESS_CODE1 || results.value == BUTTON_PRESS_CODE2) {
+        if (results.value == BUTTON1_PRESS_CODE1 || results.value == BUTTON1_PRESS_CODE2) {
             Serial.println("Matched! Toggling LED.");
             digitalWrite(LED_PIN, HIGH);
             delay(1000);
             digitalWrite(LED_PIN, LOW);
-        } else {
+        }
+        
+        else if (results.value == BUTTON_PRESS_UP) {
+
+          liftup();
+        }
+
+        else if (results.value == BUTTON_PRESS_STOP1 || results.value == BUTTON_PRESS_STOP2) {
+
+          liftstop();
+        }
+
+
+        
+        
+        else {
             Serial.println("Received code did not match expected code.");
         }
         irrecv.resume();
     }
     delay(100); // Small delay to avoid flooding the Serial monitor
 }
+
+//Create your own functions or modify them as per your project requirements
+
+void liftup(){
+  Serial.println("LIFT IS GOING UP.");
+}
+
+void liftstop(){
+  Serial.println("LIFT IS STOPED.");
+}
+
+void liftdown(){
+  Serial.println("LIFT IS GOING DOWN.");
+}
+
